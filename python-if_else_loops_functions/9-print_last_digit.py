8a2f5e7e56fed10f04ed0673c8204860ba424f18
Diesos <ozturk@live.Fr>
2024-01-23 23:29:53 +0100

    Add new Python files and modify existing file

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,8 @@ def print_last_digit(number):
     if number < 0:
         number = number * (-1)
         print(number % 10, end="")
+        return(number % 10)
 
     else:
         print(number % 10, end="")
+        return(number % 10)
