f7f23d3c4f87e6cf9990813957dee0cb757de7f9
Diesos <ozturk@live.Fr>
2024-01-23 20:13:57 +0100

    Add Python if-else, loops, and functions exercises

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of ", number," is ", end="")
+if number < 0:
+    print("-", end="")
+    number = number * (-1)
+if number > 10:
+    last_Digit = number % 10
+if number > 0 and number < 9:
+    last_Digit = number
+if last_Digit < 6 and (last_Digit != 0):
+     print(last_Digit, " and is less than 6 and not 0")
+if last_Digit > 5:
+    print(last_Digit, " and is greater than 5")
+if last_Digit == 0:
+    print(last_Digit, " and is 0")
+
+# What did i learn with this project ?
+    #I learnt that in Python each "Print" function adds automatically a New Line...
+    # Sor for stopping this behaviour we need to add at the end of print :
+    # end="" . Doing this will append each print line together.
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+i = 97
+while i < 123:
+    if i == 101 or i == 113:
+        i += 1
+    print(chr(i), end="")
+    i += 1
