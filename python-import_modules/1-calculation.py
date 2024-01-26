69aa342710bfe9cd710a19a3925a51d42a9c9561
Diesos <ozturk@live.Fr>
2024-01-26 22:08:25 +0100

    Refactor import statements and improve code readability

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,29 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-"""
-Tests the add, sub, mul, and div functions from the calculator_1 module.
-
-Usage:
-- Run the script to perform basic arithmetic operations
-using the specified functions.
-"""
-
-add = __import__("calculator_1").add
-sub = __import__("calculator_1").sub
-mul = __import__("calculator_1").mul
-div = __import__("calculator_1").div
 a = 10
 b = 5
-print(f"{a} + {b} = ", add(a, b))
-print(f"{a} - {b} = ", sub(a, b))
-print(f"{a} * {b} = ", mul(a, b))
-print(f"{a} / {b} = ", div(a, b))
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
 
-    a = 10
-    b = 5
+if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+
 import sys
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
-    numberOfArguments = len(arguments)
-    result = 0
+    addition = 0
 
-    if numberOfArguments != 0:
-        result = sum(int(arg) for arg in arguments)
-    print("{}".format(result))
+    for numbers in arguments:
+        addition += int(numbers)
+    print(addition)
