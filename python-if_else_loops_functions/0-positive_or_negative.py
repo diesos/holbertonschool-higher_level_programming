37956a809931e3d57c8894befe8e702489b82342
Diesos <ozturk@live.Fr>
2024-01-26 21:47:27 +0100

    Refactor code to improve readability and remove unnecessary comments

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-
-"""
-Generates a random integer between -10 and 10 say if positif or negtve or null
-
-Usage:
-- Run the script to display if negative, positive, or zero.
-"""
-
 import random
+
 number = random.randint(-10, 10)
-if number < 0:
-    print(number, " is negative\n")
 if number > 0:
-    print(number, " is positive\n")
-if number == 0:
-    print(number, " is zero\n")
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,35 +1,14 @@
 #!/usr/bin/python3
-
-"""
-Generates a random integer between -10000 and 10000
-analyzes and prints info about its last digit.
-
-Usage:
-- Run the script to display information about
- the last digit of a randomly generated number.
-"""
-
 import random
 
 number = random.randint(-10000, 10000)
-print("Last digit of ", number, " is ", end="")
+last_digit = abs(number) % 10
 if number < 0:
-    print("-", end="")
-    number = number * (-1)
-if number > 10:
-    last_Digit = number % 10
-if number > 0 and number < 9:
-    last_Digit = number
-if last_Digit < 6 and (last_Digit != 0):
-    print(last_Digit, " and is less than 6 and not 0")
-if last_Digit > 5:
-    print(last_Digit, " and is greater than 5")
-if last_Digit == 0:
-    print(last_Digit, " and is 0")
-
-# What did i learn with this project ?
-# I learnt that in Python each "Print"
-# function adds automatically a New Line...
-# Sor for stopping this behaviour we need to add
-# at the end of print :
-# end="" . Doing this will append each print line together.
+    last_digit = -last_digit
+if last_digit > 5:
+    message = "and is greater than 5"
+elif last_digit == 0:
+    message = "and is 0"
+else:
+    message = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} {message}")
