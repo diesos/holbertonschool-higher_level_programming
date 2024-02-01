e26ded7e5781bc2fa0ca33296472509747548cea
Diesos <ozturk@live.Fr>
2024-02-01 09:19:44 +0100

    Refactor code to improve performance and readability

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,8 @@
 def element_at(my_list, idx):
     if idx > len(my_list) or idx < 0:
         return None
-    for i in range(0,idx + 1):
+    for i in range(0, idx + 1):
         i += 1
         if i == idx:
             return my_list[i]
+        
