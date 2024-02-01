a3c70f363576bdc56fa5313cbf155b7fd5f867bd
Diesos <ozturk@live.Fr>
2024-02-01 09:21:53 +0100

    Fix index out of range bug in element_at function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@ def element_at(my_list, idx):
     if idx > len(my_list) or idx < 0:
         return None
     for i in range(0, idx + 1):
-        i += 1
         if i == idx:
             return my_list[i]
-        
+        i += 1
