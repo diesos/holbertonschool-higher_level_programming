47fe5c892f66268f3c8b094a8ba1bdb81d954d68
Diesos <ozturk@live.Fr>
2024-02-02 22:40:54 +0100

    Refactor multiply_by_2 function to use a temporary dictionary
    This commit refactors the multiply_by_2 function in 9-multiply_by_2.py to use a temporary dictionary for iteration and updates the values by multiplying them by 2. This improves code readability and maintainability.

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    
+    temp_dict = dict(a_dictionary)
+    for key, value in temp_dict.items():
+        temp_dict[key] = value * 2
+    return temp_dict
