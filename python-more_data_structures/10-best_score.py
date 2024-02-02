8470d5ae0f3fb513ad0234e6f2357db55a4b3c01
Diesos <ozturk@live.Fr>
2024-02-02 22:45:59 +0100

    Refactor best_score function to handle empty dictionaries and None values

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    
+    if not a_dictionary:  # Check if the dictionary is empty
+        return None
+
+    max_value = float("-inf")  # Initialize max_value to negative infinity
+
+    for key, value in a_dictionary.items():
+        if value is not None:  # Check if the value is not None
+            if max_value < value:
+                max_value = value
+
+    return max_value
