eab060c1b6333aec719b8ff87d2b453b51be1522
Diesos <ozturk@live.Fr>
2024-02-02 14:53:25 +0100

    Refactor multiple_returns function to simplify code

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if not sentence:
         return None
-    else:
-        longeur = ""
-        first = ""
-        longeur = len(sentence)
-        first = sentence[0]
-        return (longeur, first)
+
+    return len(sentence), sentence[0]
