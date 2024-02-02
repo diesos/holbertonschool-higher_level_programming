f22b007371b67433524ff7a2c583f2177fe75091
Diesos <ozturk@live.Fr>
2024-02-02 21:49:19 +0100

    Add search_replace function to replace elements in a list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for index, value in enumerate(my_list):
+        if value == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[index])
+    return new_list
