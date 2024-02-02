01a9eb2150214e3f0ba98df35241add2eb24d28e
Diesos <ozturk@live.Fr>
2024-02-02 14:22:49 +0100

    Refactor print_matrix_integer function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end=" ")
+    for row in range(len(matrix)):
+        for element in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][element]), end="")
+            if element != (len(matrix[row]) - 1):
+                print(" ", end="")
         print()
