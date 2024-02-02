174742b0676304163b26423abe90422034a009fe
Diesos <ozturk@live.Fr>
2024-02-02 21:40:38 +0100

    Refactor square_matrix_simple function to use exponentiation operator

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,23 @@
-# Content of 0-square_matrix_simple.py
+#!/usr/bin/python3
+
 
 def square_matrix_simple(matrix=None):
     if matrix is None:
         matrix = []
+
+    # Create a new matrix to store squared values
     squared_matrix = []
+
+    # Iterate through rows in the original matrix
     for row in range(len(matrix)):
         squared_row = []
+
+        # Iterate through elements in the current row
         for element in range(len(matrix[row])):
-            square = pow(matrix[row][element], 2)
+            square = matrix[row][element] ** 2
             squared_row.append(square)
+
+        # Append the squared row to the new matrix
         squared_matrix.append(squared_row)
+
     return squared_matrix
