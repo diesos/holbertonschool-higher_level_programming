e8dda429d5e6f56d2c1545d7ae6d81c1236aa968
Diesos <ozturk@live.Fr>
2024-02-02 15:36:58 +0100

    Refactor max_integer function to use a loop for finding the maximum value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None or not my_list:
         return None
-    return max(my_list)
+    max_Value = my_list[0]
+    for num in my_list:
+        if num > max_Value:
+            max_Value = num
+    return max_Value
