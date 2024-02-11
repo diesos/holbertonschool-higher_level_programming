5c0f209890d7433e6c444c2a161da21af8d8698d
Diesos <ozturk@live.Fr>
2024-02-11 17:07:04 +0100

    Refactor Square class documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""
-Module 0-square
-Defines class Square
-"""
+"""Defines a class Square"""
 
 
 class Square:
-    """Empty class"""
+    """Class that defines properties of square."""
     pass
