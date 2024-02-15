452d0b8285d1081d80da72dd459fe4d2d343d366
Diesos <ozturk@live.Fr>
2024-02-15 14:45:06 +0100

    Add lookup function to retrieve attributes and methods using dir()

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""
+Defines a 'lookup' function to retrieve attributes and methods of an object using 'dir()'.
+"""
 def lookup(obj):
+    """Returns a list of attributes and methods associated with the given object."""
     return dir(obj)
