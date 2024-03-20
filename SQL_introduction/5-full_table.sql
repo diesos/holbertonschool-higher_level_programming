7afa0f5d6e8c229828dd909bffd180be8d2ccc97
Diesos <ozturk@live.Fr>
2024-03-20 17:11:16 +0100

    Refactor SQL code to create a full table with specified columns

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- This file contains the SQL code to create a table with all the columns of the first_table.
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_COMMENT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- Create a full table with the following columns:
+SHOW CREATE TABLE your_table_name;
