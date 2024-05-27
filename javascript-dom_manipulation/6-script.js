709a9b0c04b3a805d8b946743665015b993bbcef
Diesos <ozturk@live.Fr>
2024-05-27 17:07:57 +0200

    feat: Update character name in DOM

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+const charEl = document.getElementById("character")
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+.then(res=> res.json())
+.then(data => {
+	charEl.textContent = data.name;
+})
